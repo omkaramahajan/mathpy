a63399b8c8698f29d9c6234fd6aa34c5e4740c04
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2025-11-15 15:34:46 +0530

    Add files via upload

---
+++ Assignment_5.py
@@ -0,0 +1,20 @@
+#Assignment 5
+
+#Divided difference table:
+
+from math import *
+from numpy import *
+
+n=eval(input("Enter the length of x as n="))
+x=eval(input("Enter the values of x="))
+y=eval(input("Enter the values of y="))
+D=zeros((n,n+1))
+
+for i in range(0,n):
+       D[i,0]=x[i]
+       D[i,1]=y[i]
+for j in range(2,n+1):
+       for i in range(0,n-j+1):
+           D[i,j]=(D[i+1,j-1]-D[i,j-1])/(x[i+j-1]-x[i])
+print("Divided difference table is as follows:")
+print(D)
