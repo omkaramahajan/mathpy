48f99222e836b485a8cad9645e0a101d80b6aea7
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2026-05-29 21:41:51 +0530

    Add files via upload

--- slip18.py
+++ slip18.py
@@ -1,92 +1,103 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.optimize import bisect
-from math import *
+#Q1
+'''from matplotlib.pyplot import *
+ages=[21,29,35,21,41,36,32,24,43,27,38,34,31,22,39,26,34,26,44,37,37,31,30,22,39]
+bins=[20,25,30,35,40,45]
+hist(ages,bins,color='maroon',rwidth=0.8)
+xlabel('Age Groups')
+ylabel('Number of Donors')
+title('Histogram of Blood Donors')
+show()'''
+
+#Q2
+'''from matplotlib.pyplot import *
 from numpy import *
-#Newton raphson method
-def f(x):
-    return(x**3 + 7*x**2 + 9 )
-def g(x):
-    return(3*x**2 + 14*x)
-a=eval(input("Enter the value of a="))
-b=eval(input("Enter the value of b="))
-n=eval(input("Enter the value of n="))
+x=linspace(-7,10,100)
+y=2*x**2+6*x+10
+plot(x,y,color='blue')
+xlabel('x')
+ylabel('y')
+title('Graph of y = 2x^2 + 6x + 10')
+show()'''
 
-x0=(a+b)/2
-for i in range(1,n+1):
-    print([1,x0])
-    x1=x0-(f(x0)/g(x0))
-    if abs(x1-x0)<0.0001:
-        print("Accurate root is:",x0)
-        break
-    x0=x1
-
-#lagrange's interpolation
-from math import*
-from numpy import*
-n=eval(input("Enter the value of n="))
-x=eval(input("Enter the value of x="))
-y=eval(input("Enter the value of y="))
-xg=eval(input("Enter the value of xg="))
-y_val=0
-for i in range (n):
-     l=1
-     for j in range (n):
-          if j!=1:
-               l=l*(xg-x[j])/(x[i]-x[j])
-     y_val=y_val+y[i]*l
-print("value of function at xg is=",y_val)
-#backward difference table
-x=eval(input("Enter the value of x = "))
-y=eval(input("Enter the value of y = "))
-n=eval(input("Enter the value of n = "))
-D = zeros((n,n+1))
-for i in range (0,n):
-    D[i,0] = x[i]
-    D[i,1] = y[i]
-for j in range (2,n+1):
-    for i in range (n-1, j-2,-1):
-        D[i,j] = D[i,j-1]-D[i-1,j-1]
-print("Backward difference table is : ")
-print(D)
-#symphsons 3/8
-def f(x):
-     y=3*x**2 + 2
-     return(y)
-a=eval(input("Enter the value of a="))
-b=eval(input("Enter the value of b="))
-n=eval(input("Enter the value of n="))
-h=(b-a)/(n)
-sum1=0
-sum2=0
-sum3=0
-for i in range(0,n+1):
-     x=a+i*h
-     y=f(x)
-     print([x,f(x)])
-     if i==0 or i==n:
-          sum1=sum1+y
-     elif i%3==0:
-          sum2=sum2+y
-     else:
-          sum3=sum3+y
-          I=3*(h/8)*(sum1+2*sum2+3*sum3)
-print("value of definite integration=",I)
-# runge kutta 4th 
+#Q3
+'''from matplotlib.pyplot import *
+from numpy import *
 def f(x,y):
-     z= x**2 * y**2 
-     return(z)
-x0=0
-y0=2
-h=0.2
-n=2
-for i in range (1,n+1):
-     k1=h*f(x0,y0)
-     k2=h*f(x0+h/2,y0+k1/2)
-     k3=h*f(x0+h/2,y0+k2/2)
-     k4=h*f(x0+h,y0+k3)
-     x1=x0+h
-     y1=y0+(1/6)*(k1+2*k2+2*k3+k4)
-     print([i,x1,y1])
-     x0=x1
-     y0=y1
+    return -(x**2 + y**2)
+x=linspace(-15,10,100)
+y=linspace(-15,10,100)
+X,Y=meshgrid(x,y)
+Z=f(X,Y)
+ax=axes(projection='3d')
+ax.plot_surface(X,Y,Z,color='green')
+ax.set_xlabel('x axis')
+ax.set_ylabel('y axis')
+ax.set_zlabel('z axis')
+title('Surface Plot')
+show()'''
+
+#Q4
+'''from sympy import *
+x,y=symbols('x y')
+L1=Line(Eq(x+y,2))
+L2=Line(Eq(3*x+2*y,1))
+P=L1.intersection(L2)[0]
+angle=L1.angle_between(L2)*180/pi
+print("Intersection Point =",P)
+print("Angle between lines =",angle)'''
+
+#Q5
+'''from sympy import *
+A=Point(1,3)
+B=Point(3,5)
+C=Point(5,4)
+D=Point(4,2)
+poly=Polygon(A,B,C,D)
+print("Is Convex =",poly.is_convex())
+poly2=poly.rotate(pi/3,Point(2,3))
+print("Rotated Polygon =",poly2)'''
+
+#Q2 
+'''from matplotlib.pyplot import *
+from sympy import *
+P=Point(1,2)
+Q=Point(4,2)
+R=Point(4,5)
+# Original
+plot([P.x,Q.x,R.x,P.x],[P.y,Q.y,R.y,P.y],color='blue')
+# Scaling 0.5
+T1=Matrix([[0.5,0,0],[0,0.5,0],[0,0,1]])
+P1=P.transform(T1)
+Q1=Q.transform(T1)
+R1=R.transform(T1)
+plot([P1.x,Q1.x,R1.x,P1.x],[P1.y,Q1.y,R1.y,P1.y],color='red')
+# Scaling 4
+T2=Matrix([[4,0,0],[0,4,0],[0,0,1]])
+P2=P.transform(T2)
+Q2=Q.transform(T2)
+R2=R.transform(T2)
+plot([P2.x,Q2.x,R2.x,P2.x],[P2.y,Q2.y,R2.y,P2.y],color='green')
+axhline(0,color='black')
+axvline(0,color='black')
+grid()
+title('Triangle Scaling')
+show()'''
+
+
+'''from matplotlib.pyplot import *
+from math import *
+r=10
+n=60
+theta=(2*pi)/n
+x1=r
+y1=0
+for i in range(n+1):
+    plot(x1,y1,marker='.',color='blue')
+    x2=x1*cos(theta)-y1*sin(theta)
+    y2=x1*sin(theta)+y1*cos(theta)
+    x1=x2
+    y1=y2
+xlabel('x axis')
+ylabel('y axis')
+title('Circle x^2 + y^2 = 100')
+show()'''
