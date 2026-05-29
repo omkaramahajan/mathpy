7b198aa0866d0e6071ac9cf8d4d372dd68cd8577
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2026-05-29 21:42:45 +0530

    Add files via upload

--- slip20.py
+++ slip20.py
@@ -1,101 +1,101 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.optimize import bisect
-from math import *
+#Q1
+'''from matplotlib.pyplot import *
+subjects=['History','Geography','English','Sociology','Psychology']
+students=[168,90,195,95,46]
+pie(students,labels=subjects,autopct="%1.1f%%",radius=0.7)
+title('Pie Chart of Students')
+show()'''
+
+#Q2
+'''from matplotlib.pyplot import *
 from numpy import *
-#regula falsi method
-def f(x):
-    return(x**3-x-1)
-a =eval(input("Enter the value of a:"))
-b =eval(input("Enter the value of b:"))
-n =eval(input("Enter the value of n:"))
+x=linspace(5,10,100)
+y=x**2 + 5*x
+plot(x,y,ls=':')
+xlabel('x')
+ylabel('y')
+title('Graph of y = x^2 + 5x')
+show()'''
 
-for i in range(1,n+1):
-    c = (a*f(b)-b*f(a))/(f(b)-f(a))
-    print([i,c])
-    if f(a)*f(c)<0:
-        b=c
-    elif f(c)*f(b)<0:
-        a=c
-    c1 = (a*f(b)-b*f(a))/(f(b)-f(a))
+#Q3
+'''from sympy import *
+from matplotlib.pyplot import *
+A=Point(1,1)
+B=Point(1,6)
+C=Point(6,1)
+D=Point(6,6)
+plot([A.x,B.x,D.x,C.x,A.x],[A.y,B.y,D.y,C.y,A.y],color='black')
+title('Quadrilateral ABCD')
+show()'''
 
-    if abs(c1-c)<0.0001:
-        print("Accurate root is :",c)
-        break
-#divided diffrence interpolation
-from math import*
-from numpy import*
-n=eval(input("Enter the value of n="))
-x=eval(input("Enter the value of x="))
-y=eval(input("Enter the value of y="))
-xg=eval(input("Enter the value of xg="))
-d=zeros((n,n+1))
-for i in range (0,n):
-     d[i,0]=x[i]
-     d[i,1]=y[i]
-     for j in range (2,n+1):
-          for i in range (0,n-j+1):
-               d[i,j]=(d[i+1,j-1]-d[i,j-1])/(x[i+j-1]-x[i])
-print("divided difference table as follows:")
-print(d)
-sum3=0
-pp=1
-for i in range (0,n):
-     sum3=sum3+(pp*d[0,i+1])
-     pp=pp*(xg-x[i])
-print("value of function at xg is=",sum3)
+#Q4
+'''from sympy import *
+A=Point(1,1)
+B=Point(1,4)
+C=Point(4,1)
+D=Point(4,4)
 
-#backward difference table
-x=eval(input("Enter the value of x = "))
-y=eval(input("Enter the value of y = "))
-n=eval(input("Enter the value of n = "))
-D = zeros((n,n+1))
-for i in range (0,n):
-    D[i,0] = x[i]
-    D[i,1] = y[i]
-for j in range (2,n+1):
-    for i in range (n-1, j-2,-1):
-        D[i,j] = D[i,j-1]-D[i-1,j-1]
-print("Backward difference table is : ")
-print(D)
-#symphsons 3/8
-def f(x):
-     y=x**3 + 5*x + 7
-     return(y)
-a=eval(input("Enter the value of a="))
-b=eval(input("Enter the value of b="))
-n=eval(input("Enter the value of n="))
-h=(b-a)/(n)
-sum1=0
-sum2=0
-sum3=0
-for i in range(0,n+1):
-     x=a+i*h
-     y=f(x)
-     print([x,f(x)])
-     if i==0 or i==n:
-          sum1=sum1+y
-     elif i%3==0:
-          sum2=sum2+y
-     else:
-          sum3=sum3+y
-          I=3*(h/8)*(sum1+2*sum2+3*sum3)
-print("value of definite integration=",I)
-# runge kutta 4th 
-def f(x,y):
-     z=1+y
-     return(z)
-x0=0
-y0=1.1
-h=0.1
-n=2
-for i in range (1,n+1):
-     k1=h*f(x0,y0)
-     k2=h*f(x0+h/2,y0+k1/2)
-     k3=h*f(x0+h/2,y0+k2/2)
-     k4=h*f(x0+h,y0+k3)
-     x1=x0+h
-     y1=y0+(1/6)*(k1+2*k2+2*k3+k4)
-     print([i,x1,y1])
-     x0=x1
-     y0=y1
+# Shearing matrix
+T=Matrix([[1,3,0],[7,1,0],[0,0,1]])
+A1=A.transform(T)
+B1=B.transform(T)
+C1=C.transform(T)
+D1=D.transform(T)
+print("Sheared Points =",A1,B1,C1,D1)'''
+
+#Q5
+'''from sympy import *
+A=Matrix([-3,5])
+B=Matrix([3,3])
+T=Matrix([[1,3],[4,1]])
+A1=T*A
+B1=T*B
+mid=(A1+B1)/2
+print("Midpoint of transformed line =",mid)'''
+
+#Q2 
+'''from matplotlib.pyplot import *
+from sympy import *
+A=Point(4,4)
+B=Point(2,4)
+C=Point(4,2)
+# Original
+plot([A.x,B.x,C.x,A.x],[A.y,B.y,C.y,A.y],color='blue')
+# Rotation pi/2
+T1=Matrix([[0,-1,0],[1,0,0],[0,0,1]])
+A1=A.transform(T1)
+B1=B.transform(T1)
+C1=C.transform(T1)
+# Scaling factor 3
+T2=Matrix([[3,0,0],[0,3,0],[0,0,1]])
+A2=A1.transform(T2)
+B2=B1.transform(T2)
+C2=C1.transform(T2)
+plot([A2.x,B2.x,C2.x,A2.x],[A2.y,B2.y,C2.y,A2.y],color='red')
+axhline(0,color='black')
+axvline(0,color='black')
+grid()
+title('Triangle Transformation')
+show()'''
+
+
+'''from matplotlib.pyplot import *
+from math import *
+a=8
+n=100
+xmin=0
+xmax=100
+delta=(xmax-xmin)/(n-1)
+x1=xmin
+y1=sqrt(32*x1)
+for i in range(n):
+    plot(x1,y1,marker='.',color='blue')
+    plot(x1,-y1,marker='.',color='blue')
+    x2=x1+delta
+    y2=sqrt(32*x2)
+    x1=x2
+    y1=y2
+xlabel('x axis')
+ylabel('y axis')
+title('Parabola y^2 = 32x')
+show()'''
